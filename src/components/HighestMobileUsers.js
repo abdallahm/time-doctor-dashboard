a08beac7c3afc937d1fc96683bcbdad6022a8ba4
Abdallah Mosen <abdallahm@me.com>
2017-08-05 18:59:00 +0200

    organize chart data

--- src/components/HighestMobileUsers.js
+++ src/components/HighestMobileUsers.js
@@ -1,126 +1,24 @@
 import React, {Component} from 'react';
 import Col from 'react-bootstrap/lib/Col';
+import AmCharts from '@amcharts/amcharts3-react';
 
 import Widget from '../components/Widget';
-
-const AmCharts = require("@amcharts/amcharts3-react");
-
-const config = {
-      "type": "gauge",
-      "theme": "light",
-      "axes": [{
-        "axisAlpha": 0,
-        "tickAlpha": 0,
-        "labelsEnabled": false,
-        "startValue": 0,
-        "endValue": 100,
-        "startAngle": 0,
-        "endAngle": 360,
-        "centerX": "0%",
-        "centerY": "0%",
-        "axisThickness": 10,
-        "bands": [{
-          "color": "#d8dde6",
-          "startValue": 0,
-          "endValue": 100,
-          "radius": "120%",
-          "innerRadius": "105%"
-        }, {
-          "color": "#FED144",
-          "startValue": 0,
-          "endValue": 85,
-          "radius": "120%",
-          "innerRadius": "105%",
-        },{
-          "color": "#d8dde6",
-          "startValue": 0,
-          "endValue": 100,
-          "radius": "100%",
-          "innerRadius": "85%"
-        }, {
-          "color": "#f45364",
-          "startValue": 0,
-          "endValue": 80,
-          "radius": "100%",
-          "innerRadius": "85%",
-        },{
-          "color": "#d8dde6",
-          "startValue": 0,
-          "endValue": 100,
-          "radius": "80%",
-          "innerRadius": "65%"
-        }, {
-          "color": "#24d2eb",
-          "startValue": 0,
-          "endValue": 75,
-          "radius": "80%",
-          "innerRadius": "65%",
-        },{
-          "color": "#d8dde6",
-          "startValue": 0,
-          "endValue": 100,
-          "radius": "60%",
-          "innerRadius": "45%"
-        }, {
-          "color": "#5d6dbd",
-          "startValue": 0,
-          "endValue": 70,
-          "radius": "60%",
-          "innerRadius": "45%",
-        },{
-          "color": "#d8dde6",
-          "startValue": 0,
-          "endValue": 100,
-          "radius": "40%",
-          "innerRadius": "25%"
-        }, {
-          "color": "#3fc299",
-          "startValue": 0,
-          "endValue": 40,
-          "radius": "40%",
-          "innerRadius": "25%",
-        }]
-      }],
-      "labels": [{
-        "title": "Benjamin Tyler",
-        "subtitle": "20h 45m",
-        "color": "#FED144",
-      }, {
-        "title": "Darrel Hart",
-        "subtitle": "20h 45m",
-        "color": "#f45364",
-      }, {
-        "title": "Lester Douglas",
-        "subtitle": "20h 45m",
-        "color": "#24d2eb",
-      }, {
-        "title": "Georgia Harper",
-        "subtitle": "20h 45m",
-        "color": "#5d6dbd",
-      },{
-        "title": "Olga Simpson",
-        "subtitle": "20h 45m",
-        "color": "#3fc299",
-      }]
-  };
+import HighestMobileUsersChart from '../data/HighestMobileUsersChart';
 
 const footer = (
   <div>
- 
     <div className="pull-left">
       <div className="square" style={{backgroundColor: "#56bc74"}}></div>
     </div>
     <div className="pull-left">
       <div className="title">Actual Hours</div>
     </div>
-
     <div className="pull-left padding-left-20">
       <div className="square" style={{backgroundColor: "#13834d"}}></div>
     </div>
     <div className="pull-left">
       <div className="title">Hours Worked More</div>
     </div>
-  
     <div className="clearfix"></div>
   </div>
 ) 
@@ -149,7 +47,7 @@ class HighestMobileUsers extends Component {
         <Col xs={12} md={6}>
           <div className="row labels">
           { 
-            config.labels.map((label, i) => 
+            HighestMobileUsersChart.labels.map((label, i) => 
               <Col xs={12} md={6} key={i}>
                 <div className="row">
                   <div className="pull-left">
@@ -167,7 +65,7 @@ class HighestMobileUsers extends Component {
         </Col>
         <Col xs={12} md={6}>
           <div className="chartdiv">
-            <AmCharts.React {...config} />
+            <AmCharts.React {...HighestMobileUsersChart} />
           </div>
         </Col>
       </Widget>
---
+++ src/data/HighestMobileUsersChart.js
@@ -0,0 +1,98 @@
+export default {
+  "type": "gauge",
+  "theme": "light",
+  "axes": [{
+    "axisAlpha": 0,
+    "tickAlpha": 0,
+    "labelsEnabled": false,
+    "startValue": 0,
+    "endValue": 100,
+    "startAngle": 0,
+    "endAngle": 360,
+    "centerX": "0%",
+    "centerY": "0%",
+    "axisThickness": 10,
+    "bands": [{
+      "color": "#d8dde6",
+      "startValue": 0,
+      "endValue": 100,
+      "radius": "120%",
+      "innerRadius": "105%"
+    }, {
+      "color": "#FED144",
+      "startValue": 0,
+      "endValue": 85,
+      "radius": "120%",
+      "innerRadius": "105%",
+    },{
+      "color": "#d8dde6",
+      "startValue": 0,
+      "endValue": 100,
+      "radius": "100%",
+      "innerRadius": "85%"
+    }, {
+      "color": "#f45364",
+      "startValue": 0,
+      "endValue": 80,
+      "radius": "100%",
+      "innerRadius": "85%",
+    },{
+      "color": "#d8dde6",
+      "startValue": 0,
+      "endValue": 100,
+      "radius": "80%",
+      "innerRadius": "65%"
+    }, {
+      "color": "#24d2eb",
+      "startValue": 0,
+      "endValue": 75,
+      "radius": "80%",
+      "innerRadius": "65%",
+    },{
+      "color": "#d8dde6",
+      "startValue": 0,
+      "endValue": 100,
+      "radius": "60%",
+      "innerRadius": "45%"
+    }, {
+      "color": "#5d6dbd",
+      "startValue": 0,
+      "endValue": 70,
+      "radius": "60%",
+      "innerRadius": "45%",
+    },{
+      "color": "#d8dde6",
+      "startValue": 0,
+      "endValue": 100,
+      "radius": "40%",
+      "innerRadius": "25%"
+    }, {
+      "color": "#3fc299",
+      "startValue": 0,
+      "endValue": 40,
+      "radius": "40%",
+      "innerRadius": "25%",
+    }]
+  }],
+  "labels": [{
+    "title": "Benjamin Tyler",
+    "subtitle": "20h 45m",
+    "color": "#FED144",
+  }, {
+    "title": "Darrel Hart",
+    "subtitle": "20h 45m",
+    "color": "#f45364",
+  }, {
+    "title": "Lester Douglas",
+    "subtitle": "20h 45m",
+    "color": "#24d2eb",
+  }, {
+    "title": "Georgia Harper",
+    "subtitle": "20h 45m",
+    "color": "#5d6dbd",
+  },{
+    "title": "Olga Simpson",
+    "subtitle": "20h 45m",
+    "color": "#3fc299",
+  }]
+};
